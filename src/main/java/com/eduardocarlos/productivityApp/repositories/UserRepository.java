6a7703196050e239331f18906ed939b8dfe1accc
CarlosEDsl <carloseduardoestds@gmail.com>
2024-07-05 16:07:12 -0300

    ref: repository created with JPA and 2 methods for search

---
+++ src/main/java/com/eduardocarlos/productivityApp/repositories/UserRepository.java
@@ -0,0 +1,20 @@
+package com.eduardocarlos.productivityApp.repositories;
+
+import com.eduardocarlos.productivityApp.models.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Optional;
+import java.util.UUID;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, UUID> {
+
+    @Transactional(readOnly = true)
+    Optional<User> findByName(String username);
+
+    @Transactional(readOnly = true)
+    Optional<User> findByEmail(String email);
+
+}
