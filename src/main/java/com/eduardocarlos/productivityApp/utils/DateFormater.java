35c776f6eae9ad0e1207d7838ab31d2569500795
CarlosEDsl <carloseduardoestds@gmail.com>
2024-07-12 17:33:50 -0300

    feat: localdatetime converter to month enum

--- src/main/java/com/eduardocarlos/productivityApp/utils/DateFormater.java
+++ src/main/java/com/eduardocarlos/productivityApp/utils/DateFormater.java
@@ -1,6 +1,14 @@
 package com.eduardocarlos.productivityApp.utils;
 
+import com.eduardocarlos.productivityApp.models.enums.Month;
+
+import java.time.LocalDateTime;
+
 public class DateFormater {
+
+    public static Month DateTimeToMonthEnum(LocalDateTime date){
+        return Month.fromValue(date.getMonthValue());
+    }
 
 
 
