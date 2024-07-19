396365f1189ebdbcf15686e9791334510d916c66
CarlosEDsl <carloseduardoestds@gmail.com>
2024-07-19 16:51:59 -0300

    ref: removing some dtos

--- src/main/java/com/eduardocarlos/productivityApp/models/dtos/UserLoginDTO.java
+++
@@ -1,4 +0,0 @@
-package com.eduardocarlos.productivityApp.models.dtos;
-
-public record UserLoginDTO(String email, String password){
-}
--- src/main/java/com/eduardocarlos/productivityApp/models/dtos/UserLoginResDTO.java
+++
@@ -1,4 +0,0 @@
-package com.eduardocarlos.productivityApp.models.dtos;
-
-public record UserLoginResDTO(String token, Long expiresIn0){
-}
